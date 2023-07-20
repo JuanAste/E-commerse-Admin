f6bcb8d415b64c71bfe39184beda418bee3d8637
Juan Aste <juanpabloaste00@gmail.com>
2023-07-19 22:01:28 -0300

    view, edit and create product

--- pages/products/new.jsx
+++ pages/products/new.jsx
@@ -1,5 +1,11 @@
-export default function NewProduct () {
-    return(
-        <div>Products</div>
-    )
+import Layout from "@/components/Layout";
+import ProductForm from "@/components/ProductForm";
+
+export default function NewProduct() {
+  return (
+    <Layout>
+      <h1>New Product</h1>
+      <ProductForm />
+    </Layout>
+  );
 }
