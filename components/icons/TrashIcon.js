adf0d65531763f09cba85031069656e30785b0e2
Juan Aste <juanpabloaste00@gmail.com>
2023-09-27 17:20:29 -0300

    New Icon and bg trashIcon

--- components/icons/TrashIcon.js
+++ components/icons/TrashIcon.js
@@ -1,11 +1,11 @@
-export default function TrashIcon() {
+export default function TrashIcon({ bg }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth={1.5}
-      stroke="currentColor"
+      stroke={bg || "currentColor"}
       className="w-4 h-4"
     >
       <path
